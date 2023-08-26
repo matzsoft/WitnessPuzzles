a421b2be7fc3b20177a727ca6a6412b39d512f10
Mark Johnson <markj@matzsoft.com>
2023-08-25 17:43:23 -0700

    Cleaned up the Color Codable extension.
    Reformatted, streamlined, and made more Swifty.

--- WitnessPuzzles/CGExtensions.swift
+++ WitnessPuzzles/CGExtensions.swift
@@ -6,6 +6,7 @@
 //      VERSION: 1.0
 //      CREATED: 8/25/23 12:12 PM
 
+import SwiftUI
 import Foundation
 import CoreGraphics
 
@@ -19,6 +20,7 @@ struct Point: Codable {
     }
 }
 
+// Adapted from https://gist.github.com/peterfriese/bb2fc5df202f6a15cc807bd87ff15193
 //
 //  Color+Codable.swift
 //  FirestoreCodableSamples
@@ -26,96 +28,66 @@ struct Point: Codable {
 //  Created by Peter Friese on 18.03.21.
 //
 
-import SwiftUI
-
 // Inspired by https://cocoacasts.com/from-hex-to-uicolor-and-back-in-swift
 // Make Color codable. This includes support for transparency.
 // See https://www.digitalocean.com/community/tutorials/css-hex-code-colors-alpha-values
 extension Color: Codable {
-  init(hex: String) {
-    let rgba = hex.toRGBA()
-    
-    self.init(.sRGB,
-              red: Double(rgba.r),
-              green: Double(rgba.g),
-              blue: Double(rgba.b),
-              opacity: Double(rgba.alpha))
-  }
-  
-  public init(from decoder: Decoder) throws {
-    let container = try decoder.singleValueContainer()
-    let hex = try container.decode(String.self)
-    
-    self.init(hex: hex)
-  }
-  
-  public func encode(to encoder: Encoder) throws {
-    var container = encoder.singleValueContainer()
-    try container.encode(toHex)
-  }
-  
-  var toHex: String? {
-    return toHex()
-  }
-  
-  func toHex(alpha: Bool = false) -> String? {
-    guard let components = cgColor?.components, components.count >= 3 else {
-      return nil
+    init( hex: String ) {
+        let rgba = hex.toRGBA
+        
+        self.init( .sRGBLinear, red: rgba.r, green: rgba.g, blue: rgba.b, opacity: rgba.alpha )
     }
     
-    let r = Float(components[0])
-    let g = Float(components[1])
-    let b = Float(components[2])
-    var a = Float(1.0)
-    
-    if components.count >= 4 {
-      a = Float(components[3])
+    public init( from decoder: Decoder ) throws {
+        let container = try decoder.singleValueContainer()
+        
+        self.init( hex: try container.decode( String.self ) )
     }
     
-    if alpha {
-      return String(format: "%02lX%02lX%02lX%02lX",
-                    lroundf(r * 255),
-                    lroundf(g * 255),
-                    lroundf(b * 255),
-                    lroundf(a * 255))
+    public func encode( to encoder: Encoder ) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode( toHex )
     }
-    else {
-      return String(format: "%02lX%02lX%02lX",
-                    lroundf(r * 255),
-                    lroundf(g * 255),
-                    lroundf(b * 255))
+    
+    var toHex: String? {
+        return toHex()
     }
-  }
+    
+    func toHex( alpha: Bool = false ) -> String? {
+        guard let components = cgColor?.components, components.count >= 3 else { return nil }
+        
+        let r = ( components[0] * 255 ).rounded()
+        let g = ( components[1] * 255 ).rounded()
+        let b = ( components[2] * 255 ).rounded()
+        let a = components.count < 4 ? 255.0 : ( components[3] * 255 ).rounded()
+        
+        if alpha { return String( format: "%02lX%02lX%02lX%02lX", r, g, b, a ) }
+        return String( format: "%02lX%02lX%02lX", r, g, b )
+    }
 }
 
 extension String {
-  func toRGBA() -> (r: CGFloat, g: CGFloat, b: CGFloat, alpha: CGFloat) {
-    var hexSanitized = self.trimmingCharacters(in: .whitespacesAndNewlines)
-    hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
-    
-    var rgb: UInt64 = 0
-    
-    var r: CGFloat = 0.0
-    var g: CGFloat = 0.0
-    var b: CGFloat = 0.0
-    var a: CGFloat = 1.0
-    
-    let length = hexSanitized.count
-    
-    Scanner(string: hexSanitized).scanHexInt64(&rgb)
-    
-    if length == 6 {
-      r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
-      g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
-      b = CGFloat(rgb & 0x0000FF) / 255.0
+    var toRGBA: ( r: CGFloat, g: CGFloat, b: CGFloat, alpha: CGFloat ) {
+        let trimmed = self.trimmingCharacters( in: .whitespacesAndNewlines )
+        let hexSanitized = trimmed.replacingOccurrences( of: "#", with: "" )
+        let length = hexSanitized.count
+        var rgb: UInt64 = 0
+
+        Scanner( string: hexSanitized ).scanHexInt64( &rgb )
+        
+        if length == 6 {
+            let r = CGFloat( ( rgb & 0xFF0000 ) >> 16 ) / 255.0
+            let g = CGFloat( ( rgb & 0x00FF00 ) >>  8 ) / 255.0
+            let b = CGFloat(   rgb & 0x0000FF )         / 255.0
+            return ( r, g, b, 1 )
+        }  else if length == 8 {
+            let r = CGFloat( ( rgb & 0xFF000000 ) >> 24 ) / 255.0
+            let g = CGFloat( ( rgb & 0x00FF0000 ) >> 16 ) / 255.0
+            let b = CGFloat( ( rgb & 0x0000FF00 ) >>  8 ) / 255.0
+            let a = CGFloat(   rgb & 0x000000FF )         / 255.0
+            return ( r, g, b, a )
+        }
+        
+        return ( 0, 0, 0, 1 )
     }
-    else if length == 8 {
-      r = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
-      g = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
-      b = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
-      a = CGFloat(rgb & 0x000000FF) / 255.0
-    }
-    
-    return (r, g, b, a)
-  }
 }
