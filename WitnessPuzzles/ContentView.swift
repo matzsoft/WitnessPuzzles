d9fed6161ceff8d72bad090abb9dbdae015bdc8b
Mark Johnson <markj@matzsoft.com>
2023-08-28 23:03:21 -0700

    Added the toolbar.
    1. Buttons don't do anything yet.
    2. Images are just a first draft.

--- WitnessPuzzles/ContentView.swift
+++ WitnessPuzzles/ContentView.swift
@@ -13,11 +13,41 @@ struct ContentView: View {
 
     var body: some View {
         Image( nsImage: document.nsImage )
+            .toolbar {
+                ToolbarItemGroup( placement: .automatic ) {
+                    Button( action: {}, label: { Label( "Properties", systemImage: "ruler" ) } )
+                        .labelStyle( VerticalLabelStyle() )
+                    Button( action: {}, label: { Label( "Drawing", systemImage: "photo" ) } )
+                        .labelStyle( VerticalLabelStyle() )
+                    Button( action: {}, label: { Label( "Starts", systemImage: "play" ) } )
+                        .labelStyle( VerticalLabelStyle() )
+                    Button( action: {}, label: { Label( "Finishes", systemImage: "stop" ) } )
+                        .labelStyle( VerticalLabelStyle() )
+                    Button( action: {}, label: { Label( "Gaps", systemImage: "pause" ) } )
+                        .labelStyle( VerticalLabelStyle() )
+                    Button( action: {}, label: { Label( "Missing", systemImage: "cloud" ) } )
+                        .labelStyle( VerticalLabelStyle() )
+                    Button( action: {}, label: { Label( "Hexagons", systemImage: "hexagon.fill" ) } )
+                        .labelStyle( VerticalLabelStyle() )
+                    Button( action: {}, label: { Label( "Icons", systemImage: "seal" ) } )
+                        .labelStyle( VerticalLabelStyle() )
+                }
+            }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView( document: .constant( WitnessPuzzlesDocument() ) )
+    }
+}
+
+
+struct VerticalLabelStyle: LabelStyle {
+    func makeBody( configuration: Configuration ) -> some View {
+        VStack {
+            configuration.icon.font( .headline )
+            configuration.title.font( .subheadline )
+        }
     }
 }
