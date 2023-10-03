ea42359f154f542af448ce96a66aae7205193acc
Mark Johnson <markj@matzsoft.com>
2023-10-02 20:12:45 -0700

    Hexagon tool uses sidebar for color selection.
    1. HexagonView is now stripped down to just a color picker.
    2. ContentView now uses a NavigationSplitView to display a sidebar.

--- WitnessPuzzles/Views/ContentView.swift
+++ WitnessPuzzles/Views/ContentView.swift
@@ -22,118 +22,141 @@ struct ContentView: View {
         trianglesCount: WitnessPuzzlesDocument.TrianglesCount.two,
         tetris: WitnessPuzzlesDocument.TetrisInfo()
     )
+    @State var columnVisibility = NavigationSplitViewVisibility.detailOnly
 
-    func toggleTool( _ tool: ToolType ) { selectedTool = selectedTool == tool ? nil : tool }
+    func toggleTool( _ tool: ToolType ) {
+        if selectedTool == tool {
+            selectedTool = nil
+            columnVisibility = .detailOnly
+        } else {
+            selectedTool = tool
+            switch selectedTool {
+            case .none, .properties, .starts, .finishes, .gaps, .missings:
+                columnVisibility = .detailOnly
+            case .hexagons, .icons:
+                columnVisibility = .all
+            }
+        }
+    }
     func deselectTool( _ tool: ToolType ) { if selectedTool == tool { selectedTool = nil } }
 
     var body: some View {
-        Image( document.image, scale: 1.0, label: Text(verbatim: "" ) )
-            .onTapGesture { location in
-                let point = document.toPuzzleSpace( from: location )
-                switch selectedTool {
-                case nil, .properties:
-                    break
-                case .starts:
-                    if document.startExists( point: point ) {
-                        document.removeStart( point: point )
-                    } else if document.isStartPositionOK( point: point ) {
-                        document.addStart( point: point )
-                    } else {
-                        NSSound.beep()
-                    }
-                case .finishes:
-                    if document.finishExists( point: point ) {
-                        document.removeFinish( point: point )
-                    } else if let directions =
-                                WitnessPuzzlesDocument.Finish.validDirections( for: point, in: document ) {
-                        if directions.count == 1 {
-                            document.addFinish( point: point, direction: directions.first! )
+        NavigationSplitView( columnVisibility: $columnVisibility ) {
+            switch selectedTool {
+            case .none, .properties, .starts, .finishes, .gaps, .missings:
+                Text( "No settings to display." )
+            case .hexagons:
+                HexagonView( info: $iconInfo )
+            case .icons:
+                IconView( document: $document, location: lastLocation, info: $iconInfo )
+            }
+        } detail: {
+            Image( document.image, scale: 1.0, label: Text(verbatim: "" ) )
+                .onTapGesture { location in
+                    let point = document.toPuzzleSpace( from: location )
+                    switch selectedTool {
+                    case nil, .properties:
+                        break
+                    case .starts:
+                        if document.startExists( point: point ) {
+                            document.removeStart( point: point )
+                        } else if document.isStartPositionOK( point: point ) {
+                            document.addStart( point: point )
                         } else {
-                            currentConfiguration = .finishes
-                            lastLocation = point
-                            lastDirections = directions
+                            NSSound.beep()
                         }
-                    } else {
-                        NSSound.beep()
-                    }
-                case .gaps:
-                    if document.gapExists( point: point ) {
-                        document.removeGap( point: point )
-                    } else if document.isGapPositionOK( point: point ) {
-                        document.addGap( point: point )
-                    } else {
-                        NSSound.beep()
-                    }
-                case .missings:
-                    if document.missingExists( point: point ) {
-                        document.removeMissing( point: point )
-                    } else if document.isMissingPositionOK( point: point ) {
-                        document.addMissing( point: point )
-                    } else {
-                        NSSound.beep()
-                    }
-                case .hexagons:
-                    if document.hexagonExists( point: point ) {
-                        document.removeHexagon( point: point )
-                    } else if document.isHexagonPositionOK( point: point ) {
-                        currentConfiguration = .hexagons
-                        lastLocation = point
-                    } else {
-                        NSSound.beep()
-                    }
-                case .icons:
-                    if document.iconExists( point: point ) {
-                        document.removeIcon( point: point )
-                    } else if document.isIconPositionOK( point: point ) {
-                        currentConfiguration = .icons
-                        lastLocation = point
-                    } else {
-                        NSSound.beep()
+                    case .finishes:
+                        if document.finishExists( point: point ) {
+                            document.removeFinish( point: point )
+                        } else if let directions =
+                                    WitnessPuzzlesDocument.Finish.validDirections( for: point, in: document ) {
+                            if directions.count == 1 {
+                                document.addFinish( point: point, direction: directions.first! )
+                            } else {
+                                currentConfiguration = .finishes
+                                lastLocation = point
+                                lastDirections = directions
+                            }
+                        } else {
+                            NSSound.beep()
+                        }
+                    case .gaps:
+                        if document.gapExists( point: point ) {
+                            document.removeGap( point: point )
+                        } else if document.isGapPositionOK( point: point ) {
+                            document.addGap( point: point )
+                        } else {
+                            NSSound.beep()
+                        }
+                    case .missings:
+                        if document.missingExists( point: point ) {
+                            document.removeMissing( point: point )
+                        } else if document.isMissingPositionOK( point: point ) {
+                            document.addMissing( point: point )
+                        } else {
+                            NSSound.beep()
+                        }
+                    case .hexagons:
+                        if document.hexagonExists( point: point ) {
+                            document.removeHexagon( point: point )
+                        } else if document.isHexagonPositionOK( point: point ) {
+                            lastLocation = point
+                            document.addHexagon( point: point, info: iconInfo )
+                        } else {
+                            NSSound.beep()
+                        }
+                    case .icons:
+                        if document.iconExists( point: point ) {
+                            document.removeIcon( point: point )
+                        } else if document.isIconPositionOK( point: point ) {
+                            currentConfiguration = .icons
+                            lastLocation = point
+                        } else {
+                            NSSound.beep()
+                        }
                     }
                 }
-            }
-            .toolbar {
-                ToolbarItemGroup( placement: .automatic ) {
-                    Button( action: { openWindow( id: "properties", value: windowID ) } ) {
-                        Label( "Properties", systemImage: "ruler" )
-                    }.labelStyle( VerticalLabelStyle( isSelected: selectedTool == .properties ) )
-                    Button( action: { toggleTool( .starts ) } ) {
-                        Label( "Starts", systemImage: "play" )
-                    }.labelStyle( VerticalLabelStyle( isSelected: selectedTool == .starts ) )
-                    Button( action: { toggleTool( .finishes ) } ) {
-                        Label( "Finishes", systemImage: "stop" )
-                    }.labelStyle( VerticalLabelStyle( isSelected: selectedTool == .finishes ) )
-                    Button( action: { toggleTool( .gaps ) } ) {
-                        Label( "Gaps", systemImage: "pause" )
-                    }.labelStyle( VerticalLabelStyle( isSelected: selectedTool == .gaps ) )
-                    Button( action: { toggleTool( .missings ) } ) {
-                        Label( "Missing", systemImage: "cloud" )
-                    }.labelStyle( VerticalLabelStyle( isSelected: selectedTool == .missings ) )
-                    Button( action: { toggleTool( .hexagons ) } ) {
-                        Label( "Hexagons", systemImage: "hexagon.fill" )
-                    }.labelStyle( VerticalLabelStyle( isSelected: selectedTool == .hexagons ) )
-                    Button( action: { toggleTool( .icons ) } ) {
-                        Label( "Icons", systemImage: "seal" )
-                    }.labelStyle( VerticalLabelStyle( isSelected: selectedTool == .icons ) )
+                .toolbar {
+                    ToolbarItemGroup( placement: .automatic ) {
+                        Button( action: { openWindow( id: "properties", value: windowID ) } ) {
+                            Label( "Properties", systemImage: "ruler" )
+                        }.labelStyle( VerticalLabelStyle( isSelected: selectedTool == .properties ) )
+                        Button( action: { toggleTool( .starts ) } ) {
+                            Label( "Starts", systemImage: "play" )
+                        }.labelStyle( VerticalLabelStyle( isSelected: selectedTool == .starts ) )
+                        Button( action: { toggleTool( .finishes ) } ) {
+                            Label( "Finishes", systemImage: "stop" )
+                        }.labelStyle( VerticalLabelStyle( isSelected: selectedTool == .finishes ) )
+                        Button( action: { toggleTool( .gaps ) } ) {
+                            Label( "Gaps", systemImage: "pause" )
+                        }.labelStyle( VerticalLabelStyle( isSelected: selectedTool == .gaps ) )
+                        Button( action: { toggleTool( .missings ) } ) {
+                            Label( "Missing", systemImage: "cloud" )
+                        }.labelStyle( VerticalLabelStyle( isSelected: selectedTool == .missings ) )
+                        Button( action: { toggleTool( .hexagons ) } ) {
+                            Label( "Hexagons", systemImage: "hexagon.fill" )
+                        }.labelStyle( VerticalLabelStyle( isSelected: selectedTool == .hexagons ) )
+                        Button( action: { toggleTool( .icons ) } ) {
+                            Label( "Icons", systemImage: "seal" )
+                        }.labelStyle( VerticalLabelStyle( isSelected: selectedTool == .icons ) )
+                    }
                 }
-            }
-            .sheet( item: $currentConfiguration, onDismiss: { deselectTool( .properties ) } ) { sheet in
-                switch sheet {
-                case .properties:
-                    PropertiesView( document: $document )
-                case .finishes:
-                    FinishView(
-                        document: $document, location: lastLocation,
-                        directions: lastDirections, direction: lastDirections.first!
-                    )
-                case .hexagons:
-                    HexagonView( document: $document, location: lastLocation, info: $iconInfo )
-                case .icons:
-                    IconView( document: $document, location: lastLocation, info: $iconInfo )
-                default:
-                    Text( verbatim: "No configuration for \(sheet.rawValue.capitalized)." )
+                .sheet( item: $currentConfiguration, onDismiss: { deselectTool( .properties ) } ) { sheet in
+                    switch sheet {
+                    case .properties:
+                        PropertiesView( document: $document )
+                    case .finishes:
+                        FinishView(
+                            document: $document, location: lastLocation,
+                            directions: lastDirections, direction: lastDirections.first!
+                        )
+                    case .icons:
+                        IconView( document: $document, location: lastLocation, info: $iconInfo )
+                    default:
+                        Text( verbatim: "No configuration for \(sheet.rawValue.capitalized)." )
+                    }
                 }
-            }
+        }
     }
 }
 
--- WitnessPuzzles/Views/HexagonView.swift
+++ WitnessPuzzles/Views/HexagonView.swift
@@ -9,44 +9,10 @@
 import SwiftUI
 
 struct HexagonView: View {
-    @Environment( \.presentationMode ) var presentationMode
-    @Binding var document: WitnessPuzzlesDocument
-    let location: WitnessPuzzlesDocument.Point
     @Binding var info: WitnessPuzzlesDocument.IconInfo
-    @State var color: Color
 
-    init(
-        document: Binding<WitnessPuzzlesDocument>, location: WitnessPuzzlesDocument.Point,
-        info: Binding<WitnessPuzzlesDocument.IconInfo>
-    ) {
-        self._document = document
-        self.location = location
-        self._info = info
-        self._color = State( initialValue: info.wrappedValue.color )
-    }
-    
     var body: some View {
-        VStack {
-//            Text( "Select a color for the new hexagon" )
-//            Divider()
-            ColorPicker( "Color", selection: $color )
-            Divider()
-            HStack {
-                Button( "Cancel", role: .cancel ) {
-                    if NSColorPanel.shared.isVisible { NSColorPanel.shared.orderOut( nil ) }
-                    presentationMode.wrappedValue.dismiss()
-                }
-                .keyboardShortcut( .cancelAction )
-                Spacer()
-                Button( "Done", role: .destructive ) {
-                    info.color = color
-                    document.addHexagon( point: location, info: info )
-                    if NSColorPanel.shared.isVisible { NSColorPanel.shared.orderOut( nil ) }
-                    presentationMode.wrappedValue.dismiss()
-                }
-                .keyboardShortcut( .defaultAction )
-            }
-        }
+        ColorPicker( "Color", selection: $info.color )
         .frame( alignment: .center )
         .padding( 20 )
     }
